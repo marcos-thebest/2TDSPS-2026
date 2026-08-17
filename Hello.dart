7aad905464d52b6f58c72144d0256cc2ba75a77c
marcos-thebest <contato.marcosvinicius2004@gmail.com>
2026-08-17 20:06:14 -0300

    exercicio Fiap

--- Hello.dart
+++ Hello.dart
@@ -1,3 +1,17 @@
+// Exercício em Linguagem Dart
+
 void main() {
-  print("\nOlá, eu sou o Marcos");
+
+  // Declarando variaveis
+  String nome = "Marcos Vinicius";
+  int idade = 22;
+  String curso = "2TDSPS";
+  double nota = 9.5;
+  bool matriculado = true;
+
+  print("\nNome do aluno: $nome");
+  print("Idade do aluno: $idade");
+  print("Curso do aluno: $curso");
+  print("Nota do aluno: $nota");
+  print("Matriculado: $matriculado");
 }
