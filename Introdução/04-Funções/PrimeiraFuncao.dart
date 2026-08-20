ec1ae2d70ef4f08b868ad55277f5e43b5601559b
marcos-thebest <contato.marcosvinicius2004@gmail.com>
2026-08-19 22:52:31 -0300

    Exercicios de Funções

---
+++ Introdução/04-Funções/PrimeiraFuncao.dart
@@ -0,0 +1,13 @@
+// Aprendendo sobre funções em Dart
+
+// Iniciando variável
+int soma = 0;
+
+void somar(int a, int b) {
+  soma = a * b;
+  print("${a} x ${b} = ${soma}");
+}
+
+void main() {
+  somar(24, 69);
+}
---
+++ Introdução/04-Funções/SegundaFuncao.dart
@@ -0,0 +1,10 @@
+// Função é para saudar
+void saudar(String nome) {
+  // Usar interpolação de String para inserir o parâmetro na mensagem
+  print("Olá, seja bem-vindo ${nome}!");
+}
+
+void main() {
+  saudar("Marcos Vinicius");
+  saudar("Eduardo Boni");
+}
